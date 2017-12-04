346f8195c6f5dc0e8cd0b70420799d9635a458e9
Andrew Lock <andrewlock.net@gmail.com>
2017-12-04 23:29:33 +0000

    Add security headers package

--- src/AspNetCoreInDocker.Web/Startup.cs
+++ src/AspNetCoreInDocker.Web/Startup.cs
@@ -27,6 +27,7 @@ namespace AspNetCoreInDocker.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSecurityHeaders();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
