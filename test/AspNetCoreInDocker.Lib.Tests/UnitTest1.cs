68c80c038f19cfd2e6b9cfec7e99efd1ecc19e3c
Andrew Lock <andrewlock.net@gmail.com>
2018-04-24 22:11:31 +1200

    Test adding another sample project

--- test/AspNetCoreInDocker.Lib.Tests/UnitTest1.cs
+++ test/AspNetCoreInDocker.Lib.Tests/UnitTest1.cs
@@ -1,13 +1,14 @@
 using System;
 using Xunit;
 
-namespace AspNetCoreInDocker.Web.Tests
+namespace AspNetCoreInDocker.Lib.Tests
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
+            throw new Exception("Testing that this fails the build");
         }
     }
 }
